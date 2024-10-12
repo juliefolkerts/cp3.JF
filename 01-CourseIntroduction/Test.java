10c6a4dc6f44284218bc93a23dfc520a4e372ebe
Julie Folkerts <JulieVMF@gmail.com>
2024-10-12 16:15:40 +0100

    push

---
+++ 01-CourseIntroduction/Test.java
@@ -0,0 +1,16 @@
+public class Test {
+    public static void main(String[] args) {
+        /*9.	Write a program that contains variables with your data: 
+        name, surname, age, student id, whether you are a first-year student or not,
+         field of study and the name of the university where you study. 
+         The program displays your data.  */
+        String name = "Julie";
+        String surname = "Folkerts";
+        String age = "20";
+        String studentId = "232963";
+        Boolean firstYear = true;
+        String studyFied = "Appled Informatics";
+        String uni = "UEK";
+        System.out.println(name+"\n"+surname+"\n"+age+"\n"+studentId+"\n"+firstYear+"\n"+studyFied+"\n"+uni);
+    }
+}
