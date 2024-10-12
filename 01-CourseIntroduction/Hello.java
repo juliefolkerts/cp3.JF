b4f430d4d991b6c277640743e0f30bb86c050f88
Julie Folkerts <JulieVMF@gmail.com>
2024-10-12 15:52:39 +0100

    push

--- 01-CourseIntroduction/Hello.java
+++ 01-CourseIntroduction/Hello.java
@@ -2,5 +2,6 @@ public class Hello {
     public static void main(String[] args) {
         // generate some simple output
         System.out.println("Hello, World!");
+        System.out.println("Sup?");
     }
 }
