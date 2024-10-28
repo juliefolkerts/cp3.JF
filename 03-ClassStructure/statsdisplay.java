d0e8944949cdba79066bfb67cb9c7d9e5f5a1d9d
Julie Folkerts <JulieVMF@gmail.com>
2024-10-28 14:11:46 +0000

    push

--- 03-ClassStructure/statsdisplay.java
+++ 03-ClassStructure/statsdisplay.java
@@ -1,14 +1,12 @@
 public class statsdisplay {
     public static void main(String[] args) {
         //stats calculator = new stats();
-        int size = 
-        for (i=0, i < size, )
+        int[] numbers = {5,6,7,8,9,10};
         int Range = stats.ItemsInRange(5,10);
-        int Sum = stats.SumItems(null, 5, 10);
-        float Mean = stats.ArithmeticMean(null, 5, 10);
-        for (i=0, i < ){
-            
-        }
+        int Sum = stats.SumItems(numbers, 5, 10);
+        float Mean = stats.ArithmeticMean(numbers, 5, 10);
+
+
         System.out.println("From 5 to 10:");
         System.out.println("Amount of numbers in range: "+Range);
         System.out.println("Sum of numbers in range: "+Sum);
