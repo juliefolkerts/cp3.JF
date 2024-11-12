94fc6ecaa235c45cbb94c5f742ae8402c3e457cb
Julie Folkerts <JulieVMF@gmail.com>
2024-11-12 15:27:40 +0000

    push

---
+++ 05-MockTest/Practice.java
@@ -0,0 +1,38 @@
+//package 05-MockTest;
+
+public class Practice {
+    public static void main(String[] args) {
+        // Example of the printf statement:
+        int age = 25;
+        double height = 1.75;
+        System.out.printf("I am %d years old and %.2f meters tall.", age, height);
+        //%d is for int and %.2f is float with 2 decimal places
+        //%s for String %n for new line
+        //Math.sqrt(double a) 
+        //Returns the square root of a.
+        //Math.pow(double a, double b)
+        //Raises a to the power of b.
+        //Math.PI
+        //Represents the constant value of π (3.14159...).
+        //Math.abs(double a)
+        //Returns the absolute value of a.
+        //Math.max(double a, double b)
+        //Returns the maximum of two values.
+        //Math.min(double a, double b)
+        //Returns the minimum of two values.
+        //Math.random()
+        //Returns a random double value between 0.0 (inclusive) and 1.0 (exclusive).
+
+        //ArrayList is an array that can be changed:
+        ArrayList<String> fruits = new ArrayList<>();
+        //add(element): Adds an element to the list.
+        //get(index)
+        //Retrieves the element at the specified index.
+        //remove(index)
+        //Removes the element at the specified index.
+        //size()
+        //Returns the number of elements in the list.
+        //clear()
+        //Removes all elements from the list.
+    }
+}
