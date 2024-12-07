6bd9da9cdda1cd1dfa96c9d7073b51bcd42a9bc9
Julie Folkerts <JulieVMF@gmail.com>
2024-12-07 15:50:09 +0000

    push

--- 09-InheritanceAndPolymorphism/Main.java
+++ 09-InheritanceAndPolymorphism/Main.java
@@ -4,10 +4,10 @@ public class Main {
     public static void main(String[] args) {
         Book9 book1 = new Book9("Harper Lee", "To Kill a Mockingbird", 324);
         Audiobook audio1 = new Audiobook(book1.getAuthor(), book1.getTitle(), book1.getPages(), 100, 44);
-        Ebook ebook1 = new Ebook(book1.getTitle(), book1.getAuthor(),book1.getPages(), "E-Book-1.pdf");
+        Ebook ebook1 = new Ebook(book1.getAuthor(), book1.getTitle(),book1.getPages(), "E-Book-1.pdf");
         Book9 book2 = new Book9("John Doe", "The Great Gatsby", 180);
-        Audiobook audio2 = new Audiobook(book2.getTitle(), book2.getAuthor(),book2.getPages(), 0, 0);
-        Ebook ebook2 = new Ebook(book2.getTitle(), book2.getAuthor(),book2.getPages(), "E-Book-2.pdf");
+        Audiobook audio2 = new Audiobook(book2.getAuthor(), book2.getTitle(),book2.getPages(), 0, 0);
+        Ebook ebook2 = new Ebook(book2.getAuthor(), book2.getTite(),book2.getPages(), "E-Book-2.pdf");
 
 
         Libr library = new Libr();
