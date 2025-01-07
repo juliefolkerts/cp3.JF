8d2f35e4cc31a9fa539c45ea773b9d5eaff75307
Julie Folkerts <JulieVMF@gmail.com>
2025-01-07 18:57:50 +0000

    push

--- 13-Test3/C2.java
+++ 13-Test3/C2.java
@@ -2,20 +2,18 @@
 
 public class C2 {
     static boolean Iso(String text){
-        boolean answer = true;
         for (int i=0;i<text.length()-1;i++){
-            for (int j=1;j<text.length();j++){
-                if(text.charAt(i)==text.charAt(j)){
-                    answer = false;
-                    break;
+            for (int j=i+1;j<text.length();j++){
+                if(text.charAt(i)==text.charAt(j) && text.charAt(i) != ' '){
+                    return false;
                 }
             }
         }
-        return answer;
+        return true;
     }
     public static void main(String[] args) {
-        Iso("BLUE water");
-        Iso("blue water");
+        System.out.println(Iso("BLUE water"));
+        System.out.println(Iso("blue water"));
     }
 }
 
--- 13-Test3/C3.java
+++ 13-Test3/C3.java
@@ -1,2 +1,21 @@
-C4.java
-C
+public class C3{
+    private boolean[] arr;
+
+    public C3(boolean[] arr) {
+        this.arr = arr;
+    }
+    public int opposite(){
+        int count = 0;
+        for(int i=1;i<arr.length-1;i++){
+            if(arr[i-1]!=arr[i] && arr[i+1]!=arr[i]){
+                count++;
+            }
+        }
+        return count;
+    }
+    public static void main(String[] args) {
+        boolean[] arr1 = new boolean[] {true,false,true,false};
+        System.out.println(new C3(arr1).opposite());
+    }
+
+}
